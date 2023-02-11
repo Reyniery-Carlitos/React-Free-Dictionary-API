90ceaea9bfcfe65bd49476b2b959950fdf0002f1
Carlos Rubio <calitod91@gmail.com>
2023-02-10 20:02:00 -0600

    Modificando enlaces externos

--- src/components/FooterContent.jsx
+++ src/components/FooterContent.jsx
@@ -4,7 +4,7 @@ export const FooterContent = ({ text, title }) => {
       <h3 className='text-gray-600'> {title} </h3>
       {title === 'Source'
         ? (
-          <h4 className='text-violet-900'> <a href={text}> {text} </a> </h4>
+          <h4 className='text-violet-900'> <a href={text} target='_blank' rel='noreferrer'> {text} </a> </h4>
           )
         : (
           <h4 className='text-violet-900'> {text} </h4>
